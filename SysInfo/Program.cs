e478daa3ea1a2b12edd7d7455cc574661acd1b7f
Tannu23 <tanvi@awkword.in>
2017-01-23 11:15:28 +0530

    Validation and Registration done.

--- SysInfo/Program.cs
+++ SysInfo/Program.cs
@@ -24,7 +24,7 @@ namespace SysInfo
             }
             else
             {
-                Application.Run(new Feed());
+                Application.Run(new Main_Form());
             }
         }
     }
